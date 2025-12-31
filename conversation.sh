0d78fb1a866f6febe6a5f11f447fb7bd28d6da59
Ram Kumar <rmkm2034@gmail.com>
2026-01-01 01:43:50 +0530

    hello-world started

---
+++ conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "trump : how are you?"
+echo "ram : am good,How are you?"   
+echo "trump : Am good"
+echo "ram : Happy new Year"
---
+++ variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=trump
+PERSON2=ram
+
+echo "$PERSON1 : how are you?"
+echo "$PERSON2 : am good,How are you?"   
+echo "$PERSON1 : Am good"
+echo "$PERSON2 : Happy new Year"
