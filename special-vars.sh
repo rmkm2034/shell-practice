be11d7f9a4a9425d16ad7c43ddea60f59f126c19
Ram Kumar <rmkm2034@gmail.com>
2026-01-01 15:44:12 +0530

    hello-world started

--- special-vars.sh
+++ special-vars.sh
@@ -2,3 +2,10 @@
 
 echo "All variables passed in the script : $@"
 echo "number of variables : $#"
+echo "script name : $0"
+echo "current directory : $PWD"
+echo "home directory : $HOME"
+echo "user running in the script : $USER"
+echo "PID of the script : $$"
+sleep 10 &
+echo "PID of last command in background : $!"
