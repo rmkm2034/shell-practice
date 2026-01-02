9c9f4fa9bfeb9c0662517c52bb81398aa72c1ea7
Ram Kumar <rmkm2034@gmail.com>
2026-01-02 16:00:10 +0530

    hello-world started

--- conditions.sh
+++ conditions.sh
@@ -5,7 +5,7 @@ NUMBER=$1
 
 if [ $NUMBER -lt 10 ]
 then
-echo "Given number $NUmber is Less than 10 "
+echo "Given number $NUMBER is Less than 10 "
 else
 echo "Given number $NUMBER is greater than 10"
 fi
