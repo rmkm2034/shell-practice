2f0ede75e80fbe0b713d4f328cd9e7f5e697a13d
Ram Kumar <rmkm2034@gmail.com>
2026-01-03 21:51:09 +0530

    hello-world started

--- logs.sh
+++ logs.sh
@@ -8,7 +8,7 @@ Y="\e[33m"
 N="\e[0m"
 
 
-LOGS_Folder="/var/log/shellscript-logs"
+LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/SCRIPT_NAME.log"
 
