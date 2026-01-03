a16c5bf23b8b429a6e67ced19f2aa0ffe5efd949
Ram Kumar <rmkm2034@gmail.com>
2026-01-03 23:53:00 +0530

    hello-world started

---
+++ for-loop.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+LOGS_FOLDER="/var/log/shellscript-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo "script started executed at : $(date)" | tee -a $LOG_FILE
+
+package=("mysql" "python3" "ngnix")
+
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R Error : Please run this script with root access $N" | tee -a $LOG_FILE
+    echo $USERID tee -a &>>LOG_FILE
+    exit 1
+else
+    echo "Script running with root access"
+fi
+
+VALIDATE(){
+
+     if [ $1 -eq 0 ]
+    then
+        echo -e " $Y installation for $2 is success $N" | tee -a $LOG_FILE
+    else
+        echo "installation for $2 is failure"
+        exit 1
+    fi 
+}
+
+for package in ${(package[@])}
+do
+    dnf list installed $package  &>>LOG_FILE
+    if [ $? -ne 0 ]
+then
+    echo -e " $G $package is not installed , going to istalll now $N" | tee -a $LOG_FILE
+    dnf install $package -y  &>>LOG_FILE
+    VALIDATE $? $package 
+else
+    echo -e" $G $package already installed"  
+fi   
+done
+
+     
+    
+
