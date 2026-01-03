7307c85aaf12e11a47f302b1d172215604acecaf
Ram Kumar <rmkm2034@gmail.com>
2026-01-03 22:38:56 +0530

    hello-world started

--- logs.sh
+++ logs.sh
@@ -10,7 +10,7 @@ N="\e[0m"
 
 LOGS_FOLDER="/var/log/shellscript-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/SCRIPT_NAME.log"
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER
 echo "script started executed at : $(date)" tee -a &>>LOG_FILE
